fd2dae1d0ffd027a07d71570ce622415304000e1
Mihir Latkar <latkar.mihir@gmail.com>
2021-06-20 18:09:55 +0530

    got bot online

--- bot.py
+++ bot.py
@@ -1,1 +1,18 @@
-print("Hello World")
+import discord
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("Logged in as {0.user}".format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith("ml.hello"):
+        await message.channel.send("Hello!!!")
+    
+client.run(os.getenv('TOKEN'))
