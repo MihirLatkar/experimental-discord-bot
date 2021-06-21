63fda5b7399804d88e080d268165dadbe4559dce
Mihir Latkar <latkar.mihir@gmail.com>
2021-06-21 12:44:38 +0530

    added help command

--- bot.py
+++ bot.py
@@ -1,7 +1,19 @@
 import discord
 import os
+import requests
+import json
+import datetime
+
 
 client = discord.Client()
+
+def get_data(pin_code):
+    NextDay_Date = datetime.datetime.today() + datetime.timedelta(days=1)
+    data = requests.get(f"https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode={pin_code}&date={NextDay_Date}")
+    json_data = json.loads(data.text)
+    
+
+ 
 
 @client.event
 async def on_ready():
@@ -14,5 +26,7 @@ async def on_message(message):
     
     if message.content.startswith("ml.hello"):
         await message.channel.send("Hello!!!")
+    if message.content.startswith("ml.help"):
+        await message.channel.send("Welcome. \n ml.hello - for saying hello \n ml.play <Link> - to play songs \n ml.cowin <pin_code> - to see all slots in your area")
     
 client.run(os.getenv('TOKEN'))
