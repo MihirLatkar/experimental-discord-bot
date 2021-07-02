59d66fef76d72c268dd85dcec6f40b3e89e149bb
Mihir Latkar <latkar.mihir@gmail.com>
2021-07-03 05:07:27 +0530

    final bot commit

--- bot.py
+++ bot.py
@@ -20,8 +20,11 @@ def get_data(pin_code):
         f"https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode={pin_code}&date={date}")
     # print(f"{url=}")
     data = requests.get(url)
-    json_data = json.loads(data.text)['sessions']
+    json_data = json.loads(data.text).get("sessions")
+
     # print(json_data)
+    if json_data == None:
+        return []
     for i in range(len(json_data)):
         Vaccines.append(json_data[i]["name"] + "  |  " + json_data[i]["address"] + "  |  " + str(json_data[i]["available_capacity"]) + "  |  " + str(json_data[i]
                         ["available_capacity_dose1"]) + "  |  " + str(json_data[i]["available_capacity_dose2"]) + "  |  " + str(json_data[i]["min_age_limit"]) + "  |  " + json_data[i]["vaccine"])
@@ -42,13 +45,24 @@ async def on_message(message):
     if message.content.startswith("ml.hello"):
         await message.channel.send("Hello!!!")
     if message.content.startswith("ml.help"):
-        await message.channel.send("Welcome. \n ml.hello - for saying hello \n ml.play <Link> - to play songs \n ml.cowin <pin_code> - to see all slots in your area")
+        await message.channel.send("Welcome. \n ml.hello - for saying hello \n ml.cowin <pin_code> - to see all slots in your area")
     if message.content.startswith("ml.cowin"):
-        pin = message.content.split()[1]
-        if len(pin) == 6:
-            Vaccines = get_data(pin)
-            for i in range(len(Vaccines)):
-                await message.channel.send(Vaccines[i])
+        check = message.content.split()
+        if len(check) == 2:
+            pin = check[1]
+            if len(pin) == 6:
+                Vaccines = get_data(pin)
+                if not Vaccines:
+                    await message.channel.send(f"Invalid Pin number")
+                if len(Vaccines) == 1:
+                    await message.channel.send(f"No Slots available in {pin} :cry:")
+                    return 
+                for i in range(len(Vaccines)):
+                    await message.channel.send(Vaccines[i])
+            else:
+                await message.channel.send(f"Invalid Pin number")
+        else:
+            await message.channel.send("ml.cowin <pin_code> - to see all slots in your area")
 
 
 client.run(os.getenv('TOKEN'))
